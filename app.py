5583b55e54148b2b7861745aff5c227f3c1c1ee4
JonathanPage <41790971+JonathanPage@users.noreply.github.com>
2025-08-11 22:01:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import plotly.express as px
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
@@ -138,3 +138,4 @@ st.subheader("Dados Detalhados")
 st.dataframe(df_filtrado)
 
      
+
